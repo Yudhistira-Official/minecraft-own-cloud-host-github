22e7bb76b25c8e57c21f4040ebfd9da05563ef7e
Yudhistira-Official <80193681+Yudhistira-Official@users.noreply.github.com>
2024-05-04 15:22:03 +0000

    changes

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,3 @@
 java -jar /workspaces/minecraft-own-cloud-host-github/plugins/via.jar
-java -Xmx10G -Xms10G -jar server.jar nogui
+java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.4-49.0.49/unix_args.txt --nogui "$@"
+java -Xmx6G -Xms6G -jar server.jar nogui
